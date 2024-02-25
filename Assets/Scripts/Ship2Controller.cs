b247f4832326a2c41e8ff994766be63566c59f87
Egor <78965361+Kloun456@users.noreply.github.com>
2024-02-26 01:02:45 +0300

    feat(101): добавить второй тип врагов
    Добавлено:
    Добавлен второй тип врагов, к нему добавлены анимации и скрипты

--- Assets/Scripts/Ship2Controller.cs
+++ Assets/Scripts/Ship2Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ship1Controller : MonoBehaviour
+public class Ship2Controller : MonoBehaviour
 {
     public float speed;
     public bool isVertical;
@@ -22,18 +22,18 @@ public class Ship1Controller : MonoBehaviour
     {
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        
+
         timerAtack = timeAtack;
         animator.SetFloat("Look X", LookX);
         animator.SetFloat("Look Y", LookY);
-    }   
+    }
 
     void FixedUpdate()
     {
         Vector2 position = rigidBody.position;
-        
+
         timerAtack -= Time.deltaTime;
-        if (timerAtack < 0) 
+        if (timerAtack < 0)
         {
             timerAtack = timeAtack;
             animator.SetBool("Is Atack", true);
--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -39,18 +39,25 @@ public class SpawnEnemies : MonoBehaviour
 
                 GameObject enemyType = enemyTypes[Random.Range(0, enemyTypes.Length)];
                 GameObject enemy = Instantiate(enemyType, spawner.position, Quaternion.identity) as GameObject; // что такое Quaternion
-                Ship1Controller enemyController = enemy.GetComponent<Ship1Controller>();
-                enemyController.LookX = spawn.LookXEnemy;
-                enemyController.LookY = spawn.LookYEnemy;
-                Debug.Log(enemy);
-                enemyController.direction = spawn.direction;
-                enemyController.isVertical = spawn.isVertical;
+                Ship1Controller enemy1Controller = null;
+                Ship2Controller enemy2Controller = null;
+                switch (enemy.name)
+                {
+                    case "Ship_1(Clone)": // заменить на имя из структуры
+                        enemy1Controller = enemy.GetComponent<Ship1Controller>();
+                        enemy1Controller.LookX = spawn.LookXEnemy;
+                        enemy1Controller.LookY = spawn.LookYEnemy;
+                        break;
+                    case "Ship_2(Clone)":
+                        enemy2Controller = enemy.GetComponent<Ship2Controller>();
+                        enemy2Controller.LookX = spawn.LookXEnemy;
+                        enemy2Controller.LookY = spawn.LookYEnemy;
+                        break;
+                }
+                Debug.Log(enemy.name);
+               
                 enemy.transform.parent = transform;
                 enemies.Add(enemy);
-                
-
-
-
 
                 timerSpawnEnemies = intervalSpawn;
                 counterEnemy++;
